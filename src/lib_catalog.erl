680c504cccc5102488522ce5f2c40fab526fe532
joq62 <joq62@c50>
2024-04-03 20:04:43 +0200

    dbg

--- src/lib_catalog.erl
+++ src/lib_catalog.erl
@@ -162,6 +162,10 @@ init(RepoDir,GitPath,ApplicationDir)->
     file:del_dir_r(RepoDir),
     CloneResult=rd:call(git_handler,clone,[RepoDir,GitPath],5000),
     ?LOG_NOTICE("Clone result  ",[CloneResult]),
+
+    init:stop(),
+    timer:sleep(2000),
+
     file:del_dir_r(ApplicationDir),
     MakeDirResult=file:make_dir(ApplicationDir),
     ?LOG_NOTICE("MakeDirResult  ",[MakeDirResult]),
